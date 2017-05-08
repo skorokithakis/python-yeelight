58b3b3ef1420b2ec02cb9c4e33ad4b0beb60aef5
Mister-Espria <kevindiner@outlook.com>
2017-05-08 17:07:54 +0000

    Added additional effects

--- yeelight/transitions.py
+++ yeelight/transitions.py
@@ -1,7 +1,9 @@
 """Pre-made transitions, for your strobing pleasure."""
 
-from .flow import HSVTransition, RGBTransition, TemperatureTransition
+from .flow import HSVTransition, RGBTransition,\
+                  TemperatureTransition, SleepTransition
 from .utils import _clamp
+import random
 
 
 def disco(bpm=120):
@@ -76,4 +78,156 @@ def pulse(red, green, blue, duration=250, brightness=100):
         RGBTransition(red, green, blue, duration=duration, brightness=brightness),
         RGBTransition(red, green, blue, duration=duration, brightness=1),
     ]
+    return transitions
+
+
+def strobe_color(brightness=100):
+    """
+    Rapid flashing colors.
+
+    :param int brightness: The brightness of the transition.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        HSVTransition(240, 100, duration=50, brightness=brightness),
+        HSVTransition(60,  100, duration=50, brightness=brightness),
+        HSVTransition(330, 100, duration=50, brightness=brightness),
+        HSVTransition(0,   100, duration=50, brightness=brightness),
+        HSVTransition(173, 100, duration=50, brightness=brightness),
+        HSVTransition(30,  100, duration=50, brightness=brightness),
+    ]
+    return transitions
+
+
+def alarm(duration=250):
+    """
+    Red alarm; flashing bright red to dark red.
+
+    :param int duration: The duration between hi/lo brightness,in milliseconds.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        HSVTransition(0, 100, duration=duration, brightness=100),
+        HSVTransition(0, 100, duration=duration, brightness=60),
+    ]
+    return transitions
+
+
+def police(duration=300, brightness=100):
+    """
+    Color changes from red to blue, like police lights.
+
+    :param int duration: The duration between red and blue, in milliseconds.
+    :param int brightness: The brightness of the transition.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        RGBTransition(255, 0, 0, duration=duration, brightness=brightness),
+        RGBTransition(0, 0, 255, duration=duration, brightness=brightness),
+    ]
+    return transitions
+
+
+def police2(duration=250, brightness=100):
+    """
+    Color flashes red and then blue, like urgent police lights.
+
+    :param int duration: The duration to fade to next color, in milliseconds.
+    :param int brightness: The brightness of the transition.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        RGBTransition(255, 0, 0, brightness=brightness, duration=duration),
+        RGBTransition(255, 0, 0, brightness=1, duration=duration),
+        RGBTransition(255, 0, 0, brightness=brightness, duration=duration),
+        SleepTransition(duration=duration),
+        RGBTransition(0, 0, 255, brightness=brightness, duration=duration),
+        RGBTransition(0, 0, 255, brightness=1, duration=duration),
+        RGBTransition(0, 0, 255, brightness=brightness, duration=duration),
+        SleepTransition(duration=duration),
+    ]
+    return transitions
+
+
+def christmas(duration=250, brightness=100, sleep=3000):
+    """
+    Color changes from red to green, like christmas lights.
+
+    :param int duration: The duration between red and green, in milliseconds.
+    :param int brightness: The brightness of the transition.
+    :param int sleep: The time to sleep between colors, in milliseconds.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        HSVTransition(0, 100, duration=duration, brightness=brightness),
+        SleepTransition(duration=sleep),
+        HSVTransition(120, 100, duration=duration, brightness=brightness),
+        SleepTransition(duration=sleep),
+    ]
+    return transitions
+
+
+def rgb(duration=250, brightness=100, sleep=3000):
+    """
+    Color changes from red to green to blue.
+
+    :param int duration: The duration to fade to next color, in milliseconds.
+    :param int brightness: The brightness of the transition.
+    :param int sleep: The time to sleep between colors, in milliseconds
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    transitions = [
+        HSVTransition(0,   100, duration=duration, brightness=brightness),
+        SleepTransition(duration=sleep),
+        HSVTransition(120, 100, duration=duration, brightness=brightness),
+        SleepTransition(duration=sleep),
+        HSVTransition(240, 100, duration=duration, brightness=brightness),
+        SleepTransition(duration=sleep),
+    ]
+    return transitions
+
+
+def randomloop(duration=750, brightness=100, count=9):
+    """
+    Color changes between 9 randomly chosen colors Or less if specified.
+
+    :param int duration: The duration to fade to next color, in milliseconds.
+    :param int brightness: The brightness of the transition.
+    :param int count: The number of random chosen colors in transition.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    count = _clamp(count, 1, 9)
+    transitions = [HSVTransition(random.randint(0, 360), 100,
+                   duration=duration) for _ in range(count)]
+    return transitions
+
+
+def slowdown(duration=2000, brightness=100, count=8):
+    """
+    Changes between 8 random chosen colors with increasing transition time.
+
+    :param int duration: The duration to fade to next color, in milliseconds.
+    :param int brightness: The brightness of the transition.
+    :param int count: The number of random chosen colors in transition.
+
+    :returns: A list of transitions.
+    :rtype: list
+    """
+    count = _clamp(count, 1, 8)
+    transitions = [HSVTransition(random.randint(0, 360), 100,
+                   duration=(duration*x)) for x in range(1, count+1)]
     return transitions
