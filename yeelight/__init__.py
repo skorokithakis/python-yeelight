703765389aeed898ba6b9b011b1ac293376f1798
Stavros Korokithakis <stavros@korokithakis.net>
2017-06-02 03:27:02 +0300

    Add flit.ini

--- yeelight/__init__.py
+++ yeelight/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa
 
+"""A Python library for controlling YeeLight RGB bulbs."""
+
 from yeelight.main import Bulb, BulbType, BulbException, discover_bulbs
 from yeelight.flow import (
         Flow,
@@ -8,3 +10,5 @@ from yeelight.flow import (
         TemperatureTransition,
         SleepTransition
 )
+
+from yeelight.version import __version__
